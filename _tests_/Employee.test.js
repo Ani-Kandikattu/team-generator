4ea3261c08e21af4a78c1fe76fc6fbf0a430552c
Ani-Kandikattu <anikandikattu@gmail.com>
2023-04-05 12:44:36 -0700

    added more tests to Employee.test and fixed some typos

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -4,19 +4,42 @@ const Employee = require("../lib/employee");
 describe("Employee", () => {
   describe("Initialization", () => {
     it("should create an employee object", () => {
-      const employee = new Employee('Name', 1234, 'employee@mail.com');
+      const employee = new Employee("Name", 1234, "employee@mail.com");
 
       expect(employee.name).toEqual(expect.any(String));
       expect(employee.id).toEqual(expect.any(Number));
       expect(employee.email).toEqual(expect.any(String));
     });
   });
-  
+
   describe("getName", () => {
     it("should return the employee's name", () => {
-      const employee = new Employee('Name', 1234, 'employee@mail.com');
+      const employee = new Employee("Name", 1234, "employee@mail.com");
 
-      expect(employee.getName()).toEqual('Name');
+      expect(employee.getName()).toEqual(expect.any(String));
     });
   });
+  describe("getID", () => {
+    it("should return the id of the employee", () => {
+      const employee = new Employee("Name", 1234, "employee@mail.com");
+
+      expect(employee.getID()).toEqual(expect.any(Number));
+    })
+  })
+  
+  describe("getEmail", () => {
+    it("should return the email of the employee", () => {
+      const employee = new Employee("Name", 1234, "employee@mail.com");
+
+      expect(employee.getEmail()).toEqual(expect.any(String));
+    })
+  })
+
+  describe("getRole", () => {
+    it("should return the employee's role", () => {
+      const employee = new Employee("Name", 1234, "employee@mail.com");
+
+      expect(employee.getRole()).toEqual('Employee');
+    });
+});
 });
