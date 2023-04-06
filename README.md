e3df9bda646e90573a3ad840cbb842e1340ae623
Ani-Kandikattu <anikandikattu@gmail.com>
2023-04-05 18:04:55 -0700

    added prompts for manager and employees and credited in README

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## Description
 
 This is a team generator that uses OOP concepts, node.js and jest to create an HTML page that contains cards with each team member's information.
+
+
+## Credit
+https://stackoverflow.com/questions/23476532/check-if-string-contains-only-letters-in-javascript 
+https://stackoverflow.com/questions/1779013/check-if-string-contains-only-digits
+https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
--- index.js
+++ index.js
@@ -5,3 +5,122 @@ const generateHTML = require("../team-generator/src/generateHTML");
 const Engineer = require("../team-generator/lib/engineer");
 const Intern = require("../team-generator/lib/intern");
 const Manager = require("../team-generator/lib/manager");
+
+const employees = [];
+
+const createManager = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Enter the manager's name:",
+      validate: (input) => {
+        if (/^[a-zA-Z]+$/.test(input)) {
+          return true;
+        } else {
+          console.log("Input must be in a valid format!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "Enter the manager's ID:",
+      validate: (input) => {
+        if (/^\d+$/.test(input)) {
+          return true;
+        } else {
+          console.log("ID must be a number!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Enter the manager's email:",
+      validate: (input) => {
+        if (/\S+@\S+\.\S+/.test(input)) {
+          return true;
+        } else {
+          console.log("Input must be in valid email format!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "Enter the manager's office number:",
+      validate: (input) => {
+        if (/^\d+$/.test(input)) {
+          return true;
+        } else {
+          console.log("Office Number must be a number!");
+        }
+      },
+    },
+  ])
+  .then(managerInput => {
+    const {name, id, email, officeNumber} = managerInput;
+    const manager = new Manager(name, id, email, officeNumber);
+    
+    employees.push(manager);
+  })
+};
+
+const createEmployees = () => {
+  return inquirer.prompt ([{
+    type: 'list',
+    name: 'role',
+    message: "Choose a role for the employee: ",
+    choices: ["Intern", "Engineer"]
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Enter the manager's name:",
+    validate: (input) => {
+      if (/^[a-zA-Z]+$/.test(input)) {
+        return true;
+      } else {
+        console.log("Input must be in a valid format!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "Enter the manager's ID:",
+    validate: (input) => {
+      if (/^\d+$/.test(input)) {
+        return true;
+      } else {
+        console.log("ID must be a number!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter the manager's email:",
+    validate: (input) => {
+      if (/\S+@\S+\.\S+/.test(input)) {
+        return true;
+      } else {
+        console.log("Input must be in valid email format!");
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter the employee's github: "
+  }
+
+  ])
+}
